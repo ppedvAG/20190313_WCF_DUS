94448edd5e5810689e6b707305499f56da934f5e
André <andrerrr@outlook.de>
2019-03-13 15:33:18 +0100

    ServiceLibrary mit Fahrrädern

---
+++ HalloWCF.Host/HalloWCF.Host/IService1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace HalloWCF.Host
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        int Verdoppeln(int zahl);
+
+        [OperationContract]
+        IEnumerable<Fahrrad> GetFahrräder();
+    }
+
+    [DataContract]
+    public class Fahrrad
+    {
+        [DataMember]
+        public int AnzahlRäder { get; set; }
+
+        [DataMember]
+        public int Größe { get; set; }
+
+        [DataMember]
+        public string Hersteller { get; set; }
+
+        [DataMember]
+        public DateTime Baujahr { get; set; }
+        //geheim
+        public string Rahmennummer { get; set; }
+    }
+}
---
+++ HalloWCF.Host/HalloWCF.Host/Service1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HalloWCF.Host
+{
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return $"You entered: {value}";
+        }
+
+        public IEnumerable<Fahrrad> GetFahrräder()
+        {
+            yield return new Fahrrad()
+            {
+                AnzahlRäder = 1,
+                Größe = 5,
+                Baujahr = DateTime.Now.AddYears(-30),
+                Hersteller = "ACME",
+                Rahmennummer = "55-KF-88-33"
+            };
+
+            yield return new Fahrrad()
+            {
+                AnzahlRäder = 3,
+                Größe = 2,
+                Baujahr = DateTime.Now.AddYears(-3),
+                Hersteller = "Toy'R'Us",
+                Rahmennummer = Guid.NewGuid().ToString()
+            };
+
+            yield return new Fahrrad()
+            {
+                AnzahlRäder = 2,
+                Größe = 28,
+                Baujahr = DateTime.Now.AddYears(-3),
+                Hersteller = "Fischer",
+                Rahmennummer = Guid.NewGuid().ToString()
+            };
+        }
+
+        public int Verdoppeln(int zahl)
+        {
+            return zahl * 2;
+        }
+    }
+}
