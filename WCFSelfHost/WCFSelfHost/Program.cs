019dda6d52b88067485a3dde5ac43434d48e2da4
André <andrerrr@outlook.de>
2019-03-13 16:50:01 +0100

    ZugDepot Service mit TCP, IPC, BasicHTTP und WSHTTP

---
+++ WCFSelfHost/WCFSelfHost/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** WCF Self Host ***");
+
+            using (var host = new ServiceHost(typeof(TrainDepot)))
+            {
+                var tcp = new NetTcpBinding();
+                var smb = new ServiceMetadataBehavior()
+                {
+                    HttpGetEnabled = true,
+                    HttpGetUrl = new Uri("http://localhost:2/mex")
+                };
+                host.Description.Behaviors.Add(smb);
+
+                host.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexTcpBinding(), "net.tcp://localhost:1/mex");
+                host.AddServiceEndpoint(typeof(ITrainDepot), tcp, "net.tcp://localhost:1");
+                host.AddServiceEndpoint(typeof(ITrainDepot), new BasicHttpBinding(), "http://localhost:2");
+                host.AddServiceEndpoint(typeof(ITrainDepot), new NetNamedPipeBinding(), "net.pipe://localhost/Trains");
+                host.AddServiceEndpoint(typeof(ITrainDepot), new WSHttpBinding(), "http://localhost:3");
+
+                host.Open();
+
+                Console.WriteLine("Service wurde gestartet");
+                Console.ReadLine();
+            }
+            Console.WriteLine("Ende");
+            Console.ReadKey();
+        }
+    }
+
+    [ServiceContract]
+    public interface ITrainDepot
+    {
+        [OperationContract]
+        IEnumerable<Train> GetTrains();
+
+        [OperationContract]
+        void AddTrain(Train train);
+    }
+
+    public class TrainDepot : ITrainDepot
+    {
+        List<Train> trains = new List<Train>();
+        public TrainDepot()
+        {
+            AddTrain(new Train { Antrieb = "Elektro", Leistung = 5600, Wert = 275.99m });
+            AddTrain(new Train { Antrieb = "Diesel", Leistung = 500, Wert = 187.89m });
+            AddTrain(new Train { Antrieb = "Elektro", Leistung = 5000, Wert = 409.23m });
+
+        }
+        public void AddTrain(Train train)
+        {
+            train.Id = trains.Count();
+            trains.Add(train);
+        }
+
+        public IEnumerable<Train> GetTrains()
+        {
+            return trains;
+        }
+    }
+}
---
+++ WCFSelfHost/WCFSelfHost/Train.cs
@@ -0,0 +1,10 @@
+﻿namespace WCFSelfHost
+{
+    public class Train
+    {
+        public int Id { get; set; }
+        public string Antrieb { get; set; }
+        public double Leistung { get; set; }
+        public decimal Wert { get; set; }
+    }
+}
