a2b25eab0d9a535a065c6089d049261630823aec
André <andrerrr@outlook.de>
2019-03-14 10:51:47 +0100

    Schnatter Chat Server

---
+++ Schnattern.Contracts/Schnattern.Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using Schnattern.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schnattern.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Schnattern Server v0.1 ***");
+
+            using (var host = new ServiceHost(typeof(SchnatternServer), new Uri("net.tcp://localhost:1")))
+            {
+                var tcp = new NetTcpBinding();
+                host.AddServiceEndpoint(typeof(IServer), tcp, "");
+
+                host.Open();
+                Console.WriteLine("Server wurde gestartet");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Ende");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Schnattern.Contracts/Schnattern.Server/SchnatternServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Schnattern.Contracts;
+
+namespace Schnattern.Server
+{
+    public class SchnatternServer : IServer
+    {
+        private List<string> users = new List<string>();
+        public void Login(string username)
+        {
+            users.Add(username);
+            Console.WriteLine($"{username} hat sich eingeloggt");
+        }
+
+        public void Logout()
+        {
+            Console.WriteLine($"Logout...");
+        }
+
+        public void SendImage(Stream image)
+        {
+            Console.WriteLine($"SendImage...");
+        }
+
+        public void SendText(string text)
+        {
+            Console.WriteLine($"SendText: {text}");
+        }
+    }
+}
