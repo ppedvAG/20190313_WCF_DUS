b6ab40409c550da2a921b6c47d981a49b563f8f8
André <andrerrr@outlook.de>
2019-03-14 10:25:28 +0100

    Contracts fürs Schnattern

---
+++ Schnattern.Contracts/Schnattern.Contracts/IClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.ServiceModel;
+
+namespace Schnattern.Contracts
+{
+    [ServiceContract]
+    public interface IClient
+    {
+        [OperationContract]
+        void ShowText(string text);
+
+        [OperationContract]
+        void ShowImage(Stream image);
+
+        [OperationContract]
+        void ShowUsers(IEnumerable<string> users);
+    }
+}
